987a1ce3984c54e628605df870a78021a5caa8cd
Negin <neginsaeidi@yahoo.com>
2021-12-10 22:11:50 -0500

    Added checkPointController

--- Assets/[Scripts]/CheckpointTriggerController.cs
+++ Assets/[Scripts]/CheckpointTriggerController.cs
@@ -4,5 +4,22 @@ using UnityEngine;
 
 public class CheckpointTriggerController : MonoBehaviour
 {
-  
+    public Transform spawnPoint;
+
+    private GameController gameController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameController = GameObject.FindObjectOfType<GameController>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            gameController.SetCurrentSpawnPoint(spawnPoint);
+        }
+    }
+
 }
