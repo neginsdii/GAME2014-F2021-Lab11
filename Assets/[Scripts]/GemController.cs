f738733354e95619696cb98f892f06ec193e0099
Negin <neginsaeidi@yahoo.com>
2021-12-10 22:02:18 -0500

    Added Gem Controller

--- Assets/[Scripts]/GemController.cs
+++ Assets/[Scripts]/GemController.cs
@@ -4,5 +4,43 @@ using UnityEngine;
 
 public class GemController : MonoBehaviour
 {
-  
+    [Header("Movement")]
+    [Range(0.1f, 2.0f)]
+    public float rotationSpeed;
+
+    [Header("Audio")]
+    public AudioSource pickupSound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pickupSound = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SpinGem();
+    }
+
+    private void SpinGem()
+    {
+        transform.Rotate(Vector3.up, rotationSpeed);
+    }
+
+    private IEnumerator PlayPickupSound()
+    {
+        pickupSound.Play();
+        yield return new WaitForSeconds(0.2f);
+        Destroy(this.gameObject);
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            StartCoroutine(PlayPickupSound());
+        }
+    }
+
 }
